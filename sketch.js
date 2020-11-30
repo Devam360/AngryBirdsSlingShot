1ac86a949934d238925aaccb13e7c182151fd860
Devam360 <devamsarin@gmail.com>
2020-11-30 15:34:26 +0530

    SlingShot added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+ var constrainedLog;
+ var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +18,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+constrainedLog=new Log(230,180,80,PI/2)
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +38,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+slingshot = new SlingShot(bird.body,{x:200,y:100})  
 
 }
 
@@ -61,4 +66,15 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    
+    //line(bird.body.position.x,bird.body.position.y,constrainedLog.body.position.x,
+//constrainedLog.body.position.y)
+slingshot.display()
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+    slingshot.fly()
 }
